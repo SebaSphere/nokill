c1e5a67762099b110b3ee766c3e098b89f4056f1
SebaSphere <SebaSphereEmails@gmail.com>
2023-03-22 19:41:19 -0400

    :rocket: barebone challenge command

--- src/main/java/dev/sebastianb/nokillkillkillkill/command/challenge/ChallengeCommand.java
+++ src/main/java/dev/sebastianb/nokillkillkillkill/command/challenge/ChallengeCommand.java
@@ -3,10 +3,14 @@ package dev.sebastianb.nokillkillkillkill.command.challenge;
 import com.mojang.brigadier.Command;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import com.mojang.brigadier.context.CommandContext;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import dev.sebastianb.nokillkillkillkill.SebaUtils;
 import dev.sebastianb.nokillkillkillkill.command.ICommand;
 import net.minecraft.command.argument.EntityArgumentType;
 import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.text.Text;
 
 public class ChallengeCommand implements ICommand {
     @Override
@@ -21,8 +25,43 @@ public class ChallengeCommand implements ICommand {
                         .executes(ChallengeCommand::challengePlayer));
     }
 
-    private static int challengePlayer(CommandContext<ServerCommandSource> serverCommandSourceCommandContext) {
+    private static int challengePlayer(CommandContext<ServerCommandSource> context) {
         // TODO: do logic for challenging a player for PVP
+        // TODO: check if the challenged player currently has a pending challenge
+        try {
+            ServerPlayerEntity sourcePlayer = context.getSource().getPlayer();
+            ServerPlayerEntity challengedPlayer = EntityArgumentType.getPlayer(context, "player");
+
+            if (sourcePlayer.equals(challengedPlayer)) {
+                // if challenger is the source player, they can't challenge themselves
+                SebaUtils.ChatUtils.saySimpleMessage(context, Text.translatable("nokillkillkillkill.command.pvp.challenge.ran_as_self"));
+            } else if (ChallengeInviteTimer.invitedPlayerAndSourcePlayerUUID.contains(sourcePlayer.getUuid())) {
+                // if challenger is currently challenging another player
+                SebaUtils.ChatUtils.saySimpleMessage(context, Text.translatable("nokillkillkillkill.command.pvp.challenge.command_running"));
+            } else {
+                if (ChallengeInviteTimer.invitedPlayerAndSourcePlayerUUID.containsValue(challengedPlayer.getUuid())) {
+                    // removes source player from challenge if they accepted their challenge
+                    ChallengeInviteTimer.invitedPlayerAndSourcePlayerUUID.remove(sourcePlayer.getUuid());
+                    // TODO: do challenge logic here and enable pvp for both players
+
+                } else {
+                    // send message to source player saying they've challenged the player.
+                    SebaUtils.ChatUtils.saySimpleMessage(context, Text.translatable("nokillkillkillkill.command.pvp.challenge.invite_player_sent", challengedPlayer.getName()));
+
+                    // send message to challenged player letting them know to accept the challenge.
+                    // TODO: make command clickable
+                    challengedPlayer.sendMessage(
+                            Text.translatable("nokillkillkillkill.command.pvp.challenge.invite_player_received", sourcePlayer.getName(),
+                                    Text.translatable("nokillkillkillkill.command.pvp.challenge.invite_player_received.click_here")
+                            )
+                    );
+                    ChallengeInviteTimer.runThread(challengedPlayer, sourcePlayer, 30);
+                }
+            }
+
+        } catch (CommandSyntaxException e) {
+            throw new RuntimeException(e);
+        }
         return Command.SINGLE_SUCCESS;
     }
 
--- src/main/java/dev/sebastianb/nokillkillkillkill/command/challenge/ChallengeInviteTimer.java
+++ src/main/java/dev/sebastianb/nokillkillkillkill/command/challenge/ChallengeInviteTimer.java
@@ -10,14 +10,15 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 // A thread to check if a player is currently waiting on a challenge invite
 public class ChallengeInviteTimer implements Runnable {
-    public static volatile ConcurrentHashMap<UUID, UUID> invitedPlayerAndFactionUUID = new ConcurrentHashMap<>(); // holds player UUID and faction UUID
+    public static volatile ConcurrentHashMap<UUID, UUID> invitedPlayerAndSourcePlayerUUID = new ConcurrentHashMap<>(); // holds player UUID and challenger UUID
 
     private final ScheduledExecutorService executor;
 
     public static void runThread(ServerPlayerEntity invitedPlayer, ServerPlayerEntity challengerPlayer, int maxSecondsAlive) {
         ThreadFactory namedThreadFactory =
                 new ThreadFactoryBuilder().setNameFormat(invitedPlayer.getUuid() + "-%d").build();
-        ScheduledExecutorService executor = Executors.newScheduledThreadPool(3, namedThreadFactory); // TODO: Make a config option for amount of threads
+        // this is probably a very bad idea creating a new thread every player but "lmao" it works
+        ScheduledExecutorService executor = Executors.newScheduledThreadPool(1, namedThreadFactory);
 
         executor.scheduleAtFixedRate(new ChallengeInviteTimer(invitedPlayer, challengerPlayer, maxSecondsAlive, executor), 0, 1, TimeUnit.SECONDS);
     }
@@ -25,21 +26,24 @@ public class ChallengeInviteTimer implements Runnable {
 
     private final AtomicInteger secondsAlive;
     private final ServerPlayerEntity invitedPlayer;
+
+    private final ServerPlayerEntity challengerPlayer;
     private final UUID invitedPlayerUUID;
-    private final UUID challengerPlayer;
+    private final UUID challengerPlayerUUID;
 
     public ChallengeInviteTimer(ServerPlayerEntity invitedPlayer, ServerPlayerEntity challengerPlayer, int maxSecondsAlive, ScheduledExecutorService executor) {
         this.invitedPlayer = invitedPlayer;
+        this.challengerPlayer = challengerPlayer;
         invitedPlayerUUID = invitedPlayer.getUuid();
-        this.challengerPlayer = challengerPlayer.getUuid();
+        this.challengerPlayerUUID = challengerPlayer.getUuid();
         this.secondsAlive = new AtomicInteger(maxSecondsAlive);
         this.executor = executor;
-        invitedPlayerAndFactionUUID.putIfAbsent(this.challengerPlayer, this.challengerPlayer);
+        invitedPlayerAndSourcePlayerUUID.putIfAbsent(this.invitedPlayerUUID, this.challengerPlayerUUID);
     }
 
     @Override
     public synchronized void run() {
-        if (!invitedPlayerAndFactionUUID.containsKey(invitedPlayer.getUuid())) {
+        if (!invitedPlayerAndSourcePlayerUUID.containsKey(invitedPlayer.getUuid())) {
             executor.shutdown();
             return;
         }
@@ -48,8 +52,9 @@ public class ChallengeInviteTimer implements Runnable {
         invitedPlayer.sendMessage(Text.literal(secondsAlive.toString()), false);
 
         if (secondsAlive.decrementAndGet() < 0) {
-            invitedPlayer.sendMessage(Text.literal("Faction invite expired"), false); // TODO: replace with translatable
-            invitedPlayerAndFactionUUID.remove(invitedPlayerUUID);
+            invitedPlayer.sendMessage(Text.translatable("nokillkillkillkill.command.pvp.challenge.invite_expired_received", challengerPlayer.getName()), false);
+            challengerPlayer.sendMessage(Text.translatable("nokillkillkillkill.command.pvp.challenge.invite_expired_sent", invitedPlayer.getName()), false);
+            invitedPlayerAndSourcePlayerUUID.remove(invitedPlayerUUID);
             executor.shutdown();
         }
 
