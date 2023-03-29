ed8eafcb909a9df4a7134c6a5520ed8743eb2476
Marcelo Hernandez <marcelohdez.inq@gmail.com>
2023-03-29 16:12:56 -0400

    💄made countdown colored and say message, let user know time invite is acceptable for
    + count in 10s until final 5 seconds which are now an overlay for both players

--- src/main/java/dev/sebastianb/nokill/command/challenge/ChallengeInviteTimer.java
+++ src/main/java/dev/sebastianb/nokill/command/challenge/ChallengeInviteTimer.java
@@ -22,16 +22,18 @@ public final class ChallengeInviteTimer {
     }
 
     public static void createInvite(ServerPlayerEntity from, ServerPlayerEntity to) {
-        playerInvites.add(new InvitePair(new PlayerPair(from, to)));
+        var pair = new PlayerPair(from, to);
+        playerInvites.add(new InvitePair(pair));
+        // let invite receiver know how much time they have to respond.
+        pair.opponent()
+                .sendMessage(Text.translatable("nokill.command.pvp.challenge.inform_of_invite_time", InvitePair.SECONDS_PER_INVITE), false);
     }
 
     private static void progressInviteTimers() {
         for (var invite : playerInvites) {
             invite.secondsLeft -= 1;
 
-            invite.pair.opponent().sendMessage(Text.literal(String.valueOf(invite.secondsLeft)), false);
-
-            if (invite.secondsLeft <= 0) {
+            if (invite.secondsLeft <= 0) { // remove invite from list and let players know it expired
                 playerInvites.remove(invite);
 
                 invite.pair.challenger().sendMessage(Text.translatable(
@@ -42,6 +44,18 @@ public final class ChallengeInviteTimer {
                         "nokill.command.pvp.challenge.invite_expired_received",
                         invite.pair.challenger().getName()
                 ), false);
+
+                return;
+            }
+
+            if (invite.secondsLeft % 10 == 0) { // multiples of 5
+                invite.pair.opponent()
+                        .sendMessage(Text.translatable("nokill.command.pvp.challenge.seconds_left", invite.secondsLeft), false);
+            } else if (invite.secondsLeft <= 5) {
+                // show both players final countdown as an overlay
+                var secondsLeftOverlay = Text.literal("§c" + invite.secondsLeft);
+                invite.pair.challenger().sendMessage(secondsLeftOverlay, true);
+                invite.pair.opponent().sendMessage(secondsLeftOverlay, true);
             }
         }
     }
--- src/main/java/dev/sebastianb/nokill/command/challenge/pairstructs/InvitePair.java
+++ src/main/java/dev/sebastianb/nokill/command/challenge/pairstructs/InvitePair.java
@@ -3,8 +3,10 @@ package dev.sebastianb.nokill.command.challenge.pairstructs;
 import dev.sebastianb.nokill.command.challenge.PlayerPair;
 
 public class InvitePair {
+    public static final int SECONDS_PER_INVITE = 30;
+
     public final PlayerPair pair;
-    public int secondsLeft = 30;
+    public int secondsLeft = SECONDS_PER_INVITE;
 
     public InvitePair(PlayerPair pair) {
         this.pair = pair;
